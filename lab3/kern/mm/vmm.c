cee87ab0cb622ad3f67d2665d394a3d78dac94f4
flysoar <dark772390855@gmail.com>
2016-03-05 21:41:42 +0800

    finish lab3 1

--- lab3/kern/mm/vmm.c
+++ lab3/kern/mm/vmm.c
@@ -364,6 +364,16 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     *   mm->pgdir : the PDT of these vma
     *
     */
+    ptep=get_pte(mm->pgdir,addr,1);
+    if(ptep==NULL)
+        goto failed;
+    if(*ptep==0){
+        if(pgdir_alloc_page(mm->pgdir,addr,perm)==NULL)
+            goto failed;
+    }
+    else{
+        
+    }
 #if 0
     /*LAB3 EXERCISE 1: YOUR CODE*/
     ptep = ???              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
